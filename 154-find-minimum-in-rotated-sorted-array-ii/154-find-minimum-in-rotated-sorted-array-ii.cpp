7d04d37c697e6b03f3a34199184f6d756cc6e574
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-06-18 16:47:35 +0530

    Time: 4 ms (86.90%), Space: 12.3 MB (19.46%) - LeetHub

--- 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -9,7 +9,7 @@ public:
         while(low < high){
             mid = low + (high - low) / 2;
             if(nums[mid] > nums[high]) low = mid + 1;
-            else if(nums[mid] < nums[n-1]) high = mid;
+            else if(nums[mid] < nums[high]) high = mid;
             else high--;
         }
         return nums[low];
