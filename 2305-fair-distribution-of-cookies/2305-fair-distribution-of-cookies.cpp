890d4592c3d94fcf1a2164fbf16e2573275af70c
Ayan Kumar Pahari <pahari.ayan@gmail.com>
2022-06-19 18:19:38 +0530

    Time: 183 ms (74.39%), Space: 7 MB (21.00%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
     
-    bool isValid(int maxLimit, vector<int> &cookies, int maxChild)
-    {
-        do
-        {
+    bool isValid(vector<int>cookies, int k, int mid){
+        do{
             int child = 1;
-            int currLimit = 0;
-            int n = cookies.size();
-            for(int i=0;i<n;i++)
-            {
-                currLimit += cookies[i];
-                if(currLimit > maxLimit)
-                {
+            int sum = 0;
+            for(int i=0;i<cookies.size();i++){
+                if(cookies[i] > mid) return false;
+                
+                sum += cookies[i];
+                if(sum > mid){
                     child++;
-                    currLimit = cookies[i];
-                    
-                    if(currLimit > maxLimit)
-                        return false;
+                    sum = cookies[i];
                 }
             }
-            if(child <= maxChild)
-                return true;
+            if(child <= k) return true;
         }while(next_permutation(cookies.begin(), cookies.end()));
         return false;
     }
     
     int distributeCookies(vector<int>& cookies, int k) {
-        int n = cookies.size();
-        sort(cookies.begin(), cookies.end());
-        
-        int low = cookies[n-1];
+        sort(cookies.begin(),cookies.end());
+        int low = *max_element(cookies.begin(),cookies.end());
         int high = 0;
+        for(auto it:cookies) high += it;
         int mid, res;
-        for(int x : cookies)
-            high += x;
         
-        while(low <= high)
-        {
-            mid = low + (high - low)/2;
-            if(isValid(mid, cookies, k))
-            {
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            if(isValid(cookies, k, mid)){
                 res = mid;
                 high = mid - 1;
             }
-            else
-                low = mid + 1;
+            else low = mid + 1;
         }
         return res;
     }
